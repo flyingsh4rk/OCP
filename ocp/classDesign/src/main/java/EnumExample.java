edbe3844b52062515ff6b9a850368b917b855231
trungp <trung.pham@gbst.net>
2016-09-08 17:19:23 +0700

    Initial OCP project

---
+++ ocp/classDesign/src/main/java/EnumExample.java
@@ -0,0 +1,12 @@
+/**
+ * Created by pthanhtrung on 9/8/2016.
+ */
+public enum EnumExample {
+    ONE("ONE");
+
+    private String name;
+
+    EnumExample(String name) {
+        this.name = name;
+    }
+}
